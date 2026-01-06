41075be7544718f93cb722aa9ba3b703d3a38f91
niuzb <niuzb1@gmail.com>
2026-01-06 21:39:15 +0800

    fix: 移除cmd.sh中不再需要的hf_token参数

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,1 @@
-python -m whisperx --model tiny ~/Downloads/output.mono.aac --device cpu --compute_type int8 --hf_token "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" --no_align --diarize
+python -m whisperx --model tiny ~/Downloads/output.mono.aac --device cpu --compute_type int8 --no_align --diarize
